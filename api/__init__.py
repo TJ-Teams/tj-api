ff0bcddc65f1140b589ad589f68f1ee5a471c019
Nikita <todti@jinteki.dev>
2023-05-30 00:11:42 +0500

    fixed app launch

--- api/__init__.py
+++ api/__init__.py
@@ -5,7 +5,6 @@ from flask import Flask
 from flask_cors import CORS
 
 from .routes import rest_api, db
-from .models import init_json
 from .models import init_db
 
 app = rest_api
@@ -15,5 +14,4 @@ CORS(app)
 @app.before_first_request
 def initialize_database():
     db.create_all()
-    init_json()
     init_db()
